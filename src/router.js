70ac841c21da6e4106a1b54a5d38e65025996f20
Fredson Chaves <fredsonchaves07@gmail.com>
2020-07-13 14:14:51 -0300

    creation of the initial structure of the product registration form

--- src/router.js
+++ src/router.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const routes = express.Router()
+const products = require('./app/controllers/product')
 
 routes.get('/', (req, res) =>{
     return res.render('layout.njk')
 })
+
+routes.get('/ads/create', (req, res) => {
+    return res.redirect('/products/create')
+})
+
+
+routes.get('/products/create', products.create)
+
 
 module.exports = routes

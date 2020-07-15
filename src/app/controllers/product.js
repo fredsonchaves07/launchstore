19ee0af4476aab9149a9e0e01a1ab3ea590fd900
Fredson Chaves <fredsonchaves07@gmail.com>
2020-07-15 14:39:23 -0300

    Creating a currency mask and applying promise and async wait

--- src/app/controllers/product.js
+++ src/app/controllers/product.js
@@ -1,4 +1,5 @@
 const Category = require('../models/Category')
+const Product = require('../models/Product')
 
 module.exports = {
     create(req, res){
@@ -13,7 +14,21 @@ module.exports = {
         
     },
 
-    post(req, res){
+    async post(req, res){
+        const keys = Object.keys(req.body)
 
+        for(key of keys){
+            if(req.body[key] == ''){
+                return res.send('Fill al fields!')
+            }
+        }
+
+        let results = await Product.create(req.body)
+        const product = results.rows[0]
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render('products/create.njk', {categories, product})
     }
 }

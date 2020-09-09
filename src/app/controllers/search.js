0d6a559c5b9ad46c12b7552b77b90a84b0794367
Fredson Chaves <fredsonchaves07@gmail.com>
2020-09-09 19:22:01 -0300

    Implementation of the nunjucks search page and macros

---
+++ src/app/controllers/search.js
@@ -0,0 +1,35 @@
+const Product = require('../models/Product')
+const File = require('../models/File')
+const { formatPrice } = require('../lib/utils')
+
+module.exports = {
+    async index(req, res){
+        let results = await Product.all()
+        const products = results.rows
+
+        if(!products){
+            return res.send('Products not found')
+        }
+
+        async function getImage(productId){
+            let results = await Product.files(productId)
+            const files = results.rows.map(file => {
+                return `${req.protocol}://${req.header.host}${file.path.replace('public', '')}`
+            })
+
+            return files[0]
+        }
+
+        const productPromise = products.map(async product => {
+            product.img = await getImage(product.id)
+            product.price = formatPrice(product.old_price)
+            product.price = formatPrice(product.price)
+            
+            return product
+        }).filter((product, index) => index > 2 ? false: true)
+
+        const lastAdded = await Promise.all(productPromise)
+
+        return res.render('search/index', {product: lastAdded})
+    }
+}

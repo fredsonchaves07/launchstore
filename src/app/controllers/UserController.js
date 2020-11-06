16c94b7a3e133e4d3d206779f74882d8012af79e
Fredson Chaves <fredsonchaves07@gmail.com>
2020-11-06 13:12:52 -0300

    Criação da pagina index de usuarios

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const {formatCep, formatCpfCnpj} = require('../lib/utils')
 
 module.exports = {
     registerForm (req, res){
@@ -7,13 +8,27 @@ module.exports = {
 
     async post(req, res){
         const userId = await User.create(req.body)
-
-        req.session.userId = userId
+        console.log(req.sessio)
+        req.session.userId = 5
 
         return res.redirect('users/')
     },
 
-    show(req, res){
-        return res.send('Cadastrado')
+    async show(req, res){
+        const {userId: id} = req.session
+
+        const user = await User.findUser(id)
+
+        if(!user){
+            return render('user/register', {
+                error: 'Usuário não encontrado'
+            })
+        }
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        user.cep = formatCep(user.cep)
+
+
+        return res.render('user/index', {user})
     }
 }
